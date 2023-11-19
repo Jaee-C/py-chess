b5991c5e738ff62e8920482221698256b5b38381
Jaee-C <77240633+Jaee-C@users.noreply.github.com>
2023-11-20 10:24:22 +1100

    add github actions

--- test/test_coordinates.py
+++ test/test_coordinates.py
@@ -7,10 +7,10 @@ from chesslib.utils import BoardCoordinates
 
 class TestBoardCoordinates(unittest.TestCase):
     @parameterized.expand([
-        ("First", 1, 1, "A1"),
-        ("Last", 8, 8, "H8"),
-        ("Random1", 3, 7, "G3"),
-        ("Random2", 7, 4, "D7"),
+        ("First", 0, 0, "A1"),
+        ("Last", 7, 7, "H8"),
+        ("Random1", 2, 6, "G3"),
+        ("Random2", 6, 3, "D7"),
     ])
     def test_letter_notation(self, name, row, col, expected):
         coordinates = BoardCoordinates(row, col)
@@ -18,12 +18,12 @@ class TestBoardCoordinates(unittest.TestCase):
 
     @parameterized.expand([
         (0, -34, False),
-        (0, 0, False),
+        (0, 0, True),
         (1, 5, True),
         (BOARD_SIZE + 1, 2, False),
         (-1, BOARD_SIZE + 99, False),
         (1, 1, True),
-        (BOARD_SIZE, BOARD_SIZE, True)
+        (BOARD_SIZE, BOARD_SIZE, False)
     ])
     def test_bounds(self, row, col, expected):
         coordinates = BoardCoordinates(row, col)
